d1ca5f01447bfe065791b42ac41e7f7e849e9de3
Nikhil <nk4338996@gmail.com>
2023-02-11 12:57:28 +0530

    added some Images in image folder and added image container

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello Nikhil");
+let mountainRoad = document.getElementsByClassName('.mountainRoad');
+let spanImageText = document.getElementsByClassName('.spanImageText');
+let likeMark = document.getElementsByClassName('.likeMark');
+let addToCollectionMark = document.getElementsByClassName('.addToCollectionMark');
+mountainRoad[0].addEventListener('')
